a3f979d04b5849b84b13c841f6a19b4b31f8a1f1
epistemologist <hhrraazzaa@gmail.com>
2024-02-29 20:56:21 -0600

    More sequences

--- sloane_sequence.py
+++ sloane_sequence.py
@@ -252,6 +252,7 @@ class A000031(OEISSequence):
         else:
             return sum([arith.euler_phi(d) * pow(2, n/d) for d in arith.divisors(n)]) // n
 
+
 class A000032(OEISSequence):
     def __init__(self):
         OEISSequence.__init__(
@@ -261,12 +262,14 @@ class A000032(OEISSequence):
             description="Lucas numbers beginning at 2: L(n) = L(n-1) + L(n-2), L(0) = 2, L(1) = 1.",
             all_at_once=True
         )
+
     def _eval_up_to_n(self, n: Integer) -> List[Integer]:
-        seq = [2,1]
+        seq = [2, 1]
         while len(seq) < n:
             seq.append(seq[-1] + seq[-2])
         return seq[:n]
-    
+
+
 class A000035(OEISSequence):
     def __init__(self):
         OEISSequence.__init__(
@@ -276,10 +279,11 @@ class A000035(OEISSequence):
             description="Period 2: repeat [0, 1]; a(n) = n mod 2; parity of n.",
             all_at_once=False
         )
-    def _eval(self, n: Integer) -> Integer:
-        return n%2
 
-    
+    def _eval(self, n: Integer) -> Integer:
+        return n % 2
+
+
 class A000040(OEISSequence):
     def __init__(self):
         OEISSequence.__init__(
@@ -289,9 +293,11 @@ class A000040(OEISSequence):
             description="The prime numbers.",
             all_at_once=True,
         )
+
     def _eval_up_to_n(self, n: Integer) -> List[Integer]:
         return arith.primes_first_n(n)
-    
+
+
 class A000041(OEISSequence):
     def __init__(self):
         OEISSequence.__init__(
@@ -301,10 +307,12 @@ class A000041(OEISSequence):
             description="a(n) is the number of partitions of n (the partition numbers).",
             all_at_once=False,
         )
+
     def _eval(self, n: Integer) -> Integer:
         from sage.combinat.partition import Partitions
         return Partitions(n).cardinality()
-    
+
+
 class A000043(OEISSequence):
     def __init__(self, cached=True):
         OEISSequence.__init__(
@@ -315,10 +323,12 @@ class A000043(OEISSequence):
             all_at_once=True
         )
         self.cached = cached
+        self.SEQ_TERMS = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279, 2203, 2281, 3217, 4253, 4423, 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243, 110503,
+                          132049, 216091, 756839, 859433, 1257787, 1398269, 2976221, 3021377, 6972593, 13466917, 20996011, 24036583, 25964951, 30402457, 32582657, 37156667, 42643801, 43112609, 57885161]
+
     def _eval_up_to_n(self, n: Integer) -> List[Integer]:
         if self.cached:
-            SEQ_TERMS = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279, 2203, 2281, 3217, 4253, 4423, 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243, 110503, 132049, 216091, 756839, 859433, 1257787, 1398269, 2976221, 3021377, 6972593, 13466917, 20996011, 24036583, 25964951, 30402457, 32582657, 37156667, 42643801, 43112609, 57885161]
-            return [Integer(i) for i in SEQ_TERMS[:n]]
+            return [Integer(i) for i in self.SEQ_TERMS[:n]]
         else:
             seq = []
             for p in arith.primes(1, inf):
@@ -326,3 +336,89 @@ class A000043(OEISSequence):
                     seq.append(p)
                 if len(seq) == n:
                     return seq
+
+
+class A000045(OEISSequence):
+    def __init__(self):
+        OEISSequence.__init__(
+            self,
+            offset=0,
+            seq_number=45,
+            description="Fibonacci numbers: F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.",
+            all_at_once=False
+        )
+
+    def _eval(self, n: Integer) -> Integer:
+        from sage.combinat.combinat import fibonacci
+        return fibonacci(n)
+
+
+class A000048(OEISSequence):
+    def __init__(self):
+        OEISSequence.__init__(
+            self,
+            offset=0,
+            seq_number=48,
+            description="Number of n-bead necklaces with beads of 2 colors and primitive period n, when turning over is not allowed but the two colors can be interchanged.",
+            all_at_once=False
+        )
+
+    def _eval(self, n: Integer) -> Integer:
+        if n == 0:
+            return 1
+        else:
+            return sum([arith.moebius(d)*pow(2, n//d) for d in arith.divisors(n) if d % 2 == 1])//(2*n)
+
+
+# Some sequences dealing with the enumeration of trees ()
+class A000081(OEISSequence):
+    def __init__(self):
+        OEISSequence.__init__(
+            self,
+            offset=0,
+            seq_number=81,
+            description="Number of unlabeled rooted trees with n nodes (or connected functions with a fixed point).",
+            all_at_once=True
+        )
+    def _eval_up_to_n(self, n: Integer) -> List[Integer]:
+        # Adapted from "The Art of Computer Programming" Vol 1 2.3.4.4 Exercise 2
+        a = [0,1]
+        s = lambda n,k: sum([a[n+1-j*k] for j in range(1, n//k+1)])
+        for n_ in range(1, n):
+            a.append(sum([i * a[i] * s(n_, i) for i in range(1, n_+1)]) // n_)
+        return [Integer(i) for i in a][:n]
+
+class A000055(OEISSequence):
+    def __init__(self):
+        OEISSequence.__init__(
+            self,
+            offset=0,
+            seq_number=55,
+            description="Number of trees with n unlabeled nodes.",
+            all_at_once=True
+        )
+    def _eval_up_to_n(self, n: Integer) -> List[Integer]:
+        # Use the generating function relation 
+        # A(x) = 1 + T(x) - T(x)^2 / 2 + T(x^2) / 2, where T(x) is the g.f. of A000081 
+        terms = A000081()._eval_up_to_n(50)
+        from sage.all import PolynomialRing, ZZ
+        R = PolynomialRing(ZZ, names=('x',))
+        (x,) = R.gens()
+        T = sum([a*x**i for i, a in enumerate(terms) ] )
+        A = 1 + T - (T*T)/2 + T.subs({x: x*x})/2
+        return [Integer(i) for i in A.coefficients()[:n]]
+    
+class A000058(OEISSequence):
+    def __init__(self):
+        OEISSequence.__init__(
+            self,
+            offset=0,
+            seq_number=58,
+            description="Sylvester's sequence: a(n+1) = a(n)^2 - a(n) + 1, with a(0) = 2.",
+            all_at_once=True
+        )
+    def _eval_up_to_n(self, n: Integer) -> List:
+        seq = [2]
+        while len(seq) < n:
+            seq.append(seq[-1]**2 - seq[-1] + 1)
+        return [Integer(i) for i in seq[:n]]
